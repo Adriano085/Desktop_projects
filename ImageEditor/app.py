f39c5eac4af4a04f1d6029e0a827c0f789fb81c8
adriano085 <adrianorosa085@outlook.com>
2024-11-07 23:29:25 -0300

    Done

--- ImageEditor/app.py
+++ ImageEditor/app.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QWidget
 from image_editor import ImageEditor
+import os
 
 class Application(QWidget):
   def __init__(self):
@@ -10,6 +11,21 @@ class Application(QWidget):
 
     self.image_editor = ImageEditor()
     self.setLayout(self.image_editor.master_layout)
+
+    self.load_styles()
+  
+  def load_styles(self):
+        current_dir = os.path.dirname(__file__)
+        style_path = os.path.join(current_dir, "styles.qss")
+
+
+        # Tenta carregar o arquivo de estilo
+        try:
+            with open(style_path, "r") as file:
+                style = file.read()
+                self.setStyleSheet(style)
+        except FileNotFoundError:
+            print(f"Arquivo de estilo 'styles.qss' não encontrado em {style_path}")
 
 if __name__ == "__main__":
   app = QApplication([])
