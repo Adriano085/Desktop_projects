626763271dc2383d18f873bbdc9b8e9bfe7764da
adriano085 <adrianorosa085@outlook.com>
2024-11-21 09:43:17 -0300

    Project Completed!

--- Capstone/main.py
+++ Capstone/main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from PyQt5.QtWidgets import (
     QApplication,
@@ -12,9 +13,10 @@ from PyQt5.QtWidgets import (
     QHBoxLayout,
     QMessageBox,
     QTableWidgetItem,
+    QHeaderView,
 )
 from PyQt5.QtSql import QSqlDatabase, QSqlQuery
-from PyQt5.QtCore import QDate
+from PyQt5.QtCore import QDate, Qt
 
 
 class ExpenseApp(QWidget):
@@ -32,13 +34,15 @@ class ExpenseApp(QWidget):
         self.add_btn.clicked.connect(self.add_expense)
 
         self.delete_btn = QPushButton("Delete")
-        # self.delete_btn.clicked.connect(self.delete_expense)
+        self.delete_btn.clicked.connect(self.delete_expense)
 
         self.table = QTableWidget()
         self.table.setColumnCount(5)
         self.table.setHorizontalHeaderLabels(
             ["Id", "Date", "Category", "Amount", "Description"]
         )
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.table.sortByColumn(1, Qt.DescendingOrder)
 
         self.dropdown.addItems(
             ["Food", "Transport", "Rent", "Shopping", "Entertainment", "Bills", "Other"]
@@ -48,6 +52,10 @@ class ExpenseApp(QWidget):
         self.row1 = QHBoxLayout()
         self.row2 = QHBoxLayout()
         self.row3 = QHBoxLayout()
+
+        self.row1.setContentsMargins(5, 5, 5, 5)
+        self.row2.setContentsMargins(5, 5, 5, 5)
+        self.row3.setContentsMargins(5, 5, 5, 5)
 
         self.row1.addWidget(QLabel("Date:"))
         self.row1.addWidget(self.date_box)
@@ -70,9 +78,14 @@ class ExpenseApp(QWidget):
 
         self.setLayout(self.master_layout)
 
+        self.load_table()
+        self.load_styles()
+
     def load_table(self):
         self.table.setRowCount(0)
+
         query = QSqlQuery("SELECT * FROM expenses")
+
         row = 0
         while query.next():
             expensive_id = query.value(0)
@@ -114,6 +127,37 @@ class ExpenseApp(QWidget):
 
         self.load_table()
 
+    def delete_expense(self):
+        selected_row = self.table.currentRow()
+        if selected_row == -1:
+            QMessageBox.warning(self, "Warning", "Please select an expense to delete!.")
+            return
+
+        expensive_id = self.table.item(selected_row, 0).text()
+
+        confirm_delete = QMessageBox.question(
+            self,
+            "Confirm Delete",
+            "Are you sure you want to delete this expense?",
+            QMessageBox.Yes | QMessageBox.No,
+        )
+        if confirm_delete == QMessageBox.Yes:
+            query = QSqlQuery()
+            query.prepare("DELETE FROM expenses WHERE id = ?")
+            query.addBindValue(expensive_id)
+            query.exec_()
+
+            self.load_table()
+
+    def load_styles(self):
+        current_dir = os.path.dirname(__file__)
+        style_path = os.path.join(current_dir, "styles.qss")
+        try:
+            with open(style_path, "r") as file:
+                style = file.read()
+                self.setStyleSheet(style)
+        except FileNotFoundError:
+            print(f"Arquivo de estilo 'styles.qss' não encontrado em {style_path}")
 
 # Create Database
 database = QSqlDatabase.addDatabase("QSQLITE")
